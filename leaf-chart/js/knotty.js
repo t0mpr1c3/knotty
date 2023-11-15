00bd2f863598e6bbdc076d49cc5c9732caab9e7c
t0mpr1c3 <t0mpr1c3@gmail.com>
2023-11-15 17:35:40 +0000

    Deploying to pages from @ t0mpr1c3/knotty@fc471d51d824d6ab19d58afb103e72007f2d97bf 🚀

---
+++ leaf-chart/js/knotty.js
@@ -0,0 +1,36 @@
+function toggleVisibility(id) {
+  var panel = document.getElementById(id);
+  var check = document.getElementById(id + '_checkbox');
+  panel.style.display = (panel.style.display == 'block') ? 'none' : 'block';
+  check.checked = (panel.style.display == 'block');
+}
+
+function zoom() {
+  var slider = document.getElementById('slider');
+  var chart = document.getElementById('figure');
+  var x = slider.value;
+  chart.style.scale = x / 80 + ' ' + x / 100 * aspectRatio;
+  chart.style.transition = '0.4s';
+}
+
+function inputFloat() {
+  var span = document.getElementById('float-span');
+  var float = document.getElementById('float');
+  var button = document.getElementById('button');
+  float.style.display = 'block';
+  button.innerHTML = 'Max float length';
+  button.disabled = true;
+  span.classList.add('active');
+}
+
+function submitForm() {
+  var div = document.getElementById('resizable');
+  var input = document.getElementById('size');
+  var form = document.getElementById('form');
+  input.value = div.style.height.replace('px', '');
+  form.submit();
+}
+
+onload = (event) => {
+  zoom();
+}
